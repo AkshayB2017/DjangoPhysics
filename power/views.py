003459b01461edb71d2b7929e5ac48e54d245492
AlfinST <alfinsthomas@gmail.com>
2018-11-20 21:02:42 +0530

    Power is a lie

--- power/views.py
+++ power/views.py
@@ -12,7 +12,7 @@ def index(request):
         form = InputForm(request.POST)
         if form.is_valid():
             form2 = form.save(commit=False)
-            result = compute(float(form2.f)*form2.Funit,float(form2.a)*form2.Aunit)
+            result = compute(float(form2.f)*form2.Funit,float(form2.T)*form2.Tunit)
             
     else:
         form = InputForm()
