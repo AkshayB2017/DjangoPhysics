77da4d4431a5372f6d76427f9d5dac9c9f636f9c
AlfinST <alfinsthomas@gmail.com>
2018-11-20 20:10:36 +0530

    More Frigging Unitsgit add .

--- escapev/models.py
+++ escapev/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.forms import ModelForm
 # Create your models here.
+
+MASS=(
+    (float(1),'kg'),
+    (float(100),'tonne'),
+    (float(.001),'g')
+)
+
+DISP=(
+    (float(1),'m'),
+    (float(1000),'km'),
+    (float(0.01),'cm')
+)
+
 class Question(models.Model):
     m=models.DecimalField(max_digits=5,decimal_places=2)
+    Munit=models.FloatField(max_length=7, choices=MASS)
     r=models.DecimalField(max_digits=5,decimal_places=2)
+    Runit=models.FloatField(max_length=7, choices=DISP)
 #v= sqrt(2*G*m/r)
 class InputForm(ModelForm):
     class Meta:
--- power/models.py
+++ power/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.forms import ModelForm
 # Create your models here.
+FORCE=((float(1),'N'),
+        (float(1000),'kN')
+    )
+AREA=(  (float(1),'m2'),
+        (float(10000),'cm2'),
+        (float(1000000),'cm2')
+    )
 class Question(models.Model):
-    f=models.DecimalField(max_digits=5,decimal_places=2)
-    a=models.DecimalField(max_digits=5,decimal_places=2)
+    f=models.DecimalField(max_digits=10,decimal_places=2)
+    Funit=models.FloatField(max_length=10, choices=FORCE)
+    a=models.DecimalField(max_digits=10,decimal_places=2)
+    Aunit=models.FloatField(max_length=10, choices=AREA)
 class InputForm(ModelForm):
     class Meta:
         model=Question
